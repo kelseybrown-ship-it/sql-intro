d8906808d10f9ad160d71ec475da5d599ab4e842
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-01-16 01:12:36 +0000

    list year and name of bb team

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -7,5 +7,7 @@
 -- | 1962 | New York Mets.       | 40   | 120    |
 -- | 2003 | Detroit Tigers       | 43   | 119    |
 -- +------+----------------------+------+--------+
-
+SELECT year, name, wins, losses FROM teams 
+ORDER by losses DESC
+LIMIT 3
 
